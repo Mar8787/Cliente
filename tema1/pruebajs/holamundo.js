a9b851ef1f70efc81ce8647531ef2687adfff440
“Mar8787” <mss7@alumnos.alborfp.com>
2025-10-24 10:51:37 +0200

    Second commit initial

---
+++ tema1/pruebajs/holamundo.js
@@ -0,0 +1,29 @@
+// Comentario simple
+function saludar() {
+    alert("¡Hola mundo!");
+}
+
+/*Comentario
+multilínea*/
+
+var numero = 42;
+let decimal = 3.14;
+const PI = 3.14159;
+let texto = "Hola";
+let booleano = true;
+let nulo = null;
+let indefinido;
+let lista = [1, 2, 3, 4, 5];
+let objeto = {nombre: "Juan", edad: 30};
+let bigInt = 9007199254740991n;
+console.log(typeof(numero)); // numero
+console.log(typeof(decimal)); // decimal
+console.log(typeof(PI)); // constante
+console.log(typeof(texto)); // cadena de texto
+console.log(typeof(booleano)); // booleano
+console.log(typeof(nulo)); // objeto
+console.log(typeof(indefinido)); // indefinido
+console.log(typeof(lista)); // array
+console.log(typeof(objeto)); // objeto
+console.log(typeof(bigInt)); // bigInt
+
